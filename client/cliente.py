134228565150a62b85492266d6cf150536d130f0
ifelp <iranildofpersonal18@gmail.com>
2026-05-30 19:06:31 -0300

    feat(client): cleaning terminal to make it less dirty and messed.

--- client/cliente.py
+++ client/cliente.py
@@ -16,26 +16,33 @@ while True:
     print("3 - Não (sair)")
 
     resposta = input("-> ")
+    print("\033c", end="") #pra limpar o terminal
     
     match (resposta):
         case "1":
+            header()
             print("> Inscrever")
             print("\n")
             zona = input("Digite a sua zona: ")
             msg = f"WATCH|ZONA_{zona.upper}"
             sock.sendto(msg.encode(), server_addr)
             print("Mensagem enviada.")
+            print("\033c", end="")
 
         case "2":
+            header()
             print("> Lista de zonas disponíveis")
             print("\n")
             print("A\nB\nC\n")
+            input("Digite enter para voltar ")
+            print("\033c", end="")
 
         case "3":
             confirm = input("Você não está inscrito em nenhuma zona e a defesa civil não poderá lhe enviar alertas. Tem certeza? (s / n): ")
             if(confirm == "s" or confirm == "S"):
                 exit(0)
             else:
+                print("\033c", end="")
                 continue
 
         case _:
