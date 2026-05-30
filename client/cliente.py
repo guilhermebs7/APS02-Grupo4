7c285e6b1bf6868be4580daf771478d0903189b8
ifelp <iranildofpersonal18@gmail.com>
2026-05-30 19:22:23 -0300

    style: improving frontend styles in tui

--- client/cliente.py
+++ client/cliente.py
@@ -5,34 +5,34 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server_addr = ('localhost', 5000)
 
 def header():
-    print("------------ Sistema de Alertas da Defesa Civil ------------")
-    print("\n")
+    print("------------------------------------------------------------------------------------------------")
+    print("------------------------------ Sistema de Alertas da Defesa Civil ------------------------------")
+    print("------------------------------------------------------------------------------------------------\n")
 
 while True:
     header()
-    print("Cliente não está inscrito em nenhuma zona. Deseja inscrever-se?")
+    print("Cliente não está inscrito em nenhuma zona. Deseja inscrever-se?\n")
     print("1 - Sim")
     print("2 - Listar zonas")
     print("3 - Não (sair)")
 
     resposta = input("-> ")
-    print("\033c", end="") #pra limpar o terminal
+    if resposta != "3":
+        print("\033c", end="") #pra limpar o terminal
     
     match (resposta):
         case "1":
             header()
-            print("> Inscrever")
-            print("\n")
+            print("> Inscrever\n")
             zona = input("Digite a sua zona: ")
             msg = f"WATCH|ZONA_{zona.upper}"
             sock.sendto(msg.encode(), server_addr)
-            print("Mensagem enviada.")
             print("\033c", end="")
+            print("Mensagem enviada.")
 
         case "2":
             header()
-            print("> Lista de zonas disponíveis")
-            print("\n")
+            print("> Lista de zonas disponíveis\n")
             print("A\nB\nC\n")
             input("Digite enter para voltar ")
             print("\033c", end="")
