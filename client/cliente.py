ebc9e00bb68f587a814182a2e072d9d6a6fd2fd2
ifelp <iranildofpersonal18@gmail.com>
2026-05-31 02:23:54 -0300

    style: adding colors into the terminal interface and a alert sound on linux (arch)

--- client/cliente.py
+++ client/cliente.py
@@ -1,9 +1,20 @@
 import socket
 import threading
 from datetime import datetime
+import os
+
+os.system('color') #pro windows
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server_addr = ('localhost', 5000)
+
+RED     = "\033[1;31m"
+GREEN   = "\033[1;32m"    
+YELLOW  = "\033[1;33m"
+BLUE    = "\033[1;34m"
+MAGENTA = "\033[1;35m"
+ORANGE = "\033[1;38;5;208m"
+RESET   = "\033[0m"
 
 status = ""
 zone = ""
@@ -28,22 +39,22 @@ def get_status():
         zone = rsp.split("_")[1]
 
 def header():
-    print("------------------------------------------------------------------------------------------------")
-    print("------------------------------ Sistema de Alertas da Defesa Civil ------------------------------")
-    print("------------------------------------------------------------------------------------------------\n")
+    print(f"{ORANGE}------------------------------------------------------------------------------------------------")
+    print(f"------------------------------ Sistema de Alertas da Defesa Civil ------------------------------")
+    print(f"------------------------------------------------------------------------------------------------{RESET}\n")
 
 def unregistered_opts():
-    print("Cliente não está inscrito em nenhuma zona. Deseja inscrever-se?\n")
+    print(f"{BLUE}Cliente não está inscrito em nenhuma zona. Deseja inscrever-se?\n")
     print("1 - Sim")
     print("2 - Listar zonas")
-    print("3 - Não (sair)")
+    print(f"3 - Não (sair){RESET}")
 
 def registered_opts():
-    print("Cliente inscrito no sistema de alertas.")
-    print(f"Zona atual: {zone}\n")
-    print("1 - Ver histórico de alertas para a minha zona")
+    print(f"{GREEN}Cliente inscrito no sistema de alertas.")
+    print(f"Zona atual: {zone}{RESET}\n")
+    print(f"{BLUE}1 - Ver histórico de alertas para a minha zona")
     print("2 - Cancelar inscrição na zona atual")
-    print("3 - Sair")
+    print(f"3 - Sair{RESET}")
 
 def get_screen_state():
     match (screen_state):
@@ -54,22 +65,24 @@ def get_screen_state():
             registered_opts()
             print("-> ")
         case "UNREG_REG":
-            print("> Inscrever\n")
+            print(f"{MAGENTA}> Inscrever{RESET}\n")
             print("Digite a sua zona: ", end="")
         case "REG_HIST":
-            print(f"> Alertas para Zona {zone} ({len(alerts)} alertas):\n")
+            print(f"{MAGENTA}> Alertas para Zona {zone} ({len(alerts)} alertas):{RESET}\n")
             for alt in alerts:
                 print(alt + "\n")
             print("Aperte ENTER para voltar")
         case "UNREG_LIST":
-            print("> Lista de zonas disponíveis\n")
+            print(f"{MAGENTA}> Lista de zonas disponíveis{RESET}\n")
             print("A\nB\nC\n")
             print("Digite enter para voltar ")
         case "REG_UNSB":
-            print("> Cancelar inscrição\n")
+            print(f"{MAGENTA}> Cancelar inscrição{RESET}\n")
             print(f"Tem certeza de que deseja cancelar sua inscrição na zona {zone} (S / N)? ")
         case "UNREG_EXIT":
             print("Você não está inscrito em nenhuma zona e a defesa civil não poderá lhe enviar alertas. Tem certeza? (s / n): ")
+        case _:
+            print(f"{ORANGE}Ouvindo alertas em tempo real{RESET}")
             
 
 def check_alerts():
@@ -81,9 +94,13 @@ def check_alerts():
                 now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
                 print("\033c", end="")
                 header()
-                print(f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! {now.split(" ")[0]} !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
-                print(alert + "\n")
-                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+                print(f"{RED}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! {now.split(" ")[0]} !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!{RESET}\n")
+                print(f"{ORANGE}" + alert + f"{RESET}\n")
+                print(f"{RED}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!{RESET}\n")
+                os.system('paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga')
+                #os.system() #pra windows
+                #os.system() #pra mac
+                #os.system() #outras distros linux ???
                 alerts.append({
                     "message": alert,
                     "timestamp": now
@@ -119,21 +136,21 @@ while True:
             header()
             if status == "UNREGISTERED":
                 screen_state = "UNREG_REG"
-                print("> Inscrever\n")
+                print(f"{MAGENTA}> Inscrever{RESET}\n")
                 zona = input("Digite a sua zona: ")
                 msg = f"WATCH|Zona_{zona.upper()}"
                 sock.sendto(msg.encode(), server_addr)
                 print("\033c", end="")
-                print("Mensagem enviada.")
+                print(f"{GREEN}Mensagem enviada.{RESET}")
                 get_status()
-                alert_thread = threading.Thread(target=check_alerts, daemon=True)
+                alert_thread = threading.Thread(target=check_alerts)
                 alert_thread.start()
             else:
                 screen_state = "REG_HIST"
-                print(f"> Alertas para Zona {zone} ({len(alerts)} alertas):\n")
+                print(f"{MAGENTA}> Alertas para Zona {zone} ({len(alerts)} alertas):{RESET}\n")
                 for alt in alerts:
                     msg_line = next((line for line in alt["message"].splitlines() if line.startswith("Mensagem:")), None)
-                    print(f"[{alt["timestamp"]}]")
+                    print(f"{YELLOW}[{alt["timestamp"]}]{RESET}")
                     print(msg_line + "\n")
                 input("Aperte ENTER para voltar")
                 print("\033c", end="")
@@ -143,7 +160,7 @@ while True:
             if status == "UNREGISTERED":
                 screen_state = "UNREG_LIST"
                 header()
-                print("> Lista de zonas disponíveis\n")
+                print(f"{MAGENTA}> Lista de zonas disponíveis{RESET}\n")
                 print("A\nB\nC\n")
                 input("Digite enter para voltar ")
                 print("\033c", end="")
@@ -151,13 +168,13 @@ while True:
                 screen_state = "REG_UNSB"
                 unregister = True
                 header()
-                print("> Cancelar inscrição\n")
-                resposta = input(f"Tem certeza de que deseja cancelar sua inscrição na zona {zone} (S / N)? ")
+                print(f"{MAGENTA}> Cancelar inscrição{RESET}\n")
+                resposta = input(f"{RED}Tem certeza de que deseja cancelar sua inscrição na zona {zone} (S / N)?{RESET} ")
                 if resposta.upper() == "S":
                     msg = f"UNWATCH|Zona_{zone}"
                     sock.sendto(msg.encode(), server_addr)
                     print("\033c", end="")
-                    print("Inscrição cancelada")
+                    print(f"{RED}Inscrição cancelada{RESET}")
                     get_status()
                 else:
                     continue
@@ -168,11 +185,14 @@ while True:
                 screen_state = "UNREG_EXIT"
                 confirm = input("Você não está inscrito em nenhuma zona e a defesa civil não poderá lhe enviar alertas. Tem certeza? (s / n): ")
                 if(confirm == "s" or confirm == "S"):
+                    screen_state = ""
                     exit(0)
                 else:
                     print("\033c", end="")
                     continue
             else:
+                screen_state = ""
+                print("\033c", end="")
                 exit(0)
 
         case _:
